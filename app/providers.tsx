b0a51d2599fbcd5c4d85c2701707ac327af2bcfc
melevgeni-cpu <mel.evgeni@gmail.com>
2026-04-25 14:44:35 +0300

    Полностью Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,20 @@
 
 import { ThemeProvider } from 'next-themes'
 import { ReactLenis } from 'lenis/react'
+import Modal from '@/components/ui/Modal'
+import { useModal } from '@/hooks/useModal'
+
+function ModalWrapper() {
+  const { isOpen, type, closeModal } = useModal()
+  return <Modal isOpen={isOpen} type={type} onClose={closeModal} />
+}
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
         {children}
+        <ModalWrapper />
       </ReactLenis>
     </ThemeProvider>
   )
